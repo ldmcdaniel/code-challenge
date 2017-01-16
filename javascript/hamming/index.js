2cb0212aa1b7c0b373c91a0a7c1d9aa1eabb9601
Lucas McDaniel <ldmcdaniel@gmail.com>
2017-01-16 17:52:52 -0600

    Passing JS tests

--- javascript/hamming/index.js
+++ javascript/hamming/index.js
@@ -2,6 +2,23 @@
 // `npm test` to ensure the unit tests pass and
 // your code meets all of the conditions.
 // You may use ES6 or ES5 to solve.
+"use strict";
 
+class Hamming {
+  constructor(first, second) {
+    this.first = first;
+    this.second = second;
+  };
+
+  compute(first, second) {
+    if (first === second) {
+      return 0;
+    } else if (first.length !== second.length ){
+      throw 'DNA strands must be of equal length.';
+    } else {
+      return first.split('').filter((dna, index) => dna !== second.split('')[index]).length;
+    }
+  }
+}
 
 module.exports = Hamming;
